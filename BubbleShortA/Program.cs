7d3f92459ad9f341a1be7aa713e37cb28247f90d
IndraAlFauzan <39118928+IndraAlFauzan@users.noreply.github.com>
2022-10-05 16:47:49 +0700

    Membuat tiga hello world

--- BubbleShortA/Program.cs
+++ BubbleShortA/Program.cs
@@ -7,6 +7,8 @@ namespace BubbleShortA
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
